bed9b3b1a9af1ea6565d3e4fa04cd0f6f56bb5d1
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2023-09-01 11:24:36 +0100

    second try

--- src/components/CityCard/CityCard.tsx
+++ src/components/CityCard/CityCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import useCity from "@/hooks/useCity";
 import Card from "../Card/Card";
 import Link from "next/link";
@@ -7,27 +7,31 @@ import { getWeatherIcon } from "@/utils/getWeatherIcon";
 import { CityCardProps } from "@/types/CityCardProps";
 
 const CityCard = ({ cityName, size, color }: CityCardProps) => {
-    const { cityData, loading, error } = useCity(cityName);
+    const { cityData, error } = useCity(cityName);
+    const cachedCityData = useMemo(() => cityData, [cityData]);
 
-    if (loading) {
-        return <p>Loading...</p>;
+    if (!cachedCityData) {
+        return <div>Loading...</div>;
     }
 
     if (error) {
         return <p>Error: {error}</p>;
     }
 
-    if (cityData) {
+    if (cachedCityData) {
         return (
             <Link href={`/${cityName}`}>
                 <Card size={size} color={color}>
                     <div className="flex justify-between items-center">
                         <h4 className="font-bold text-gray-700 text-sm lg:text-lg">
-                            {cityData?.city}
+                            {cachedCityData?.city}
                         </h4>
                         <IconBox
-                            data={Math.round(cityData?.temp)}
-                            icon={getWeatherIcon(cityData?.weather, "info")}
+                            data={Math.round(cachedCityData?.temp)}
+                            icon={getWeatherIcon(
+                                cachedCityData?.weather,
+                                "info"
+                            )}
                             status={"temp"}
                             iconClasses={
                                 "flex flex-col items-center ml-6 md:ml-0"
