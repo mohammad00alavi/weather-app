d14bbaca33159c6f3602492a8fed19d367b4f3a7
Mohammad Alavi <43592603+mohammad00alavi@users.noreply.github.com>
2023-10-02 16:53:13 +0100

    added keyboard accessility to temp buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,8 +2,18 @@ import { ButtonProps } from "@/types/ButtonProps";
 import React from "react";
 
 const Button = ({ text, clickHandler, classes }: ButtonProps) => {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "Enter") {
+            clickHandler();
+        }
+    };
     return (
-        <div className={classes} onClick={clickHandler}>
+        <div
+            className={classes}
+            onClick={clickHandler}
+            tabIndex={0}
+            onKeyDown={handleKeyDown}
+        >
             {text}
         </div>
     );
